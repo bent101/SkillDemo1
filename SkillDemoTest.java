a3254436c6a2c68ea0267f5421707a38cf3df251
Ben Tomlin <57016008+bent101@users.noreply.github.com>
2022-02-04 16:42:10 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 class SkillDemoTest {
-    public static void testSkillDemo() {
+    public void testSkillDemo() {
         assertEquals(11, SkillDemo.multiply(5, 2));
     }
 }
